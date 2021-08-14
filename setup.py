96c3a29c3c6488823b12a64588c43f3134a2dd17
Emanuele Albini <7670445+emanuele-albini@users.noreply.github.com>
2021-08-14 16:56:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="qualtrutils",
-    version="0.1.0c",
+    version="0.1.1",
     author="Emanuele Albini",
     description="A simple package to interact with the Qualtrics API",
     long_description=long_description,
